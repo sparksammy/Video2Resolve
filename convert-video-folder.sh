58cd8edc49df87d491a8017404091c89e93f1648
Samuel Lord <sllord@icloud.com>
2020-01-28 15:01:20 -0600

    Update convert-video-folder.sh

--- convert-video-folder.sh
+++ convert-video-folder.sh
@@ -2,7 +2,7 @@ echo "Started Video2Resolve by Sparksammy."
 echo "Detected that you wanted the folder: $1"
 mov=".mov"
 
-for movie in "$1/"*
+for movie in "$1/"*".mp4"
 do
 ffmpeg -i "$movie" -c:v mpeg4 -q:v 5 -c:a pcm_s16le "$movie$mov"
 done
