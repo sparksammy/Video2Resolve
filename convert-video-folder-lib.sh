7eeaa641f98422c46e1f69baabe66e22d8342abb
Samuel Lord <sllord@icloud.com>
2020-01-28 15:00:04 -0600

    Add files via upload

---
+++ convert-video-folder-lib.sh
@@ -0,0 +1,8 @@
+echo "Started Video2Resolve by Sparksammy."
+echo "Detected that you wanted the parent folder: $1"
+mov=".mov"
+
+for movie in "$1/"*"/"*
+do
+ffmpeg -i "$movie" -c:v mpeg4 -q:v 5 -c:a pcm_s16le "$movie$mov"
+done
